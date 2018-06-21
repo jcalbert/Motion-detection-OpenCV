28fd6e3ca97ee1838ca00a29882f45a32f462b00
Joseph Albert <jxa357@psu.edu>
2018-06-20 22:20:59 -0400

    lots of changes to separate quantification from detection.

--- MotionDetector.py
+++ MotionDetector.py
@@ -8,17 +8,40 @@ import signal
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger()
 COOLDOWN_FRAMES = 20 # Num of non-moving frames before recording stops
 
 DOWNSAMPLE = True
 MIN_WIDTH = 256
 
-class MotionQuantifier():
+
+
+class MotionQuantifier():    
+    """
+    Class takes in a sequence of frames and tracks a per-frame level of movement.
+
+    Subclasses should maintain internal state variables necessary to track
+    movement.  They may optionally implement a visualize() method which
+    returns a frame of the same type as the input data which represents the
+    quantifier's state (e.g. highlighting fast-changing pixels).
+
+    ...
     
-    def __init__(self, init_frame):
-        self.frame = init_frame
+    Attributes
+    ----------
+    motoin_level : float
+        Number between 0 and 1 representing the current level of movement.
+
+    Methods
+    -------
+    step(frame)
+        Update the quantifier by `frame`.
+    visualize()
+        Returns a visualization of the quantifier's state.
+
+    """
+    def __init__(self):
         self.motion_level = 0.0 #All detectors quantify level of motion from 0 to 1
 
     def step(self, frame):
@@ -31,9 +54,125 @@ class MotionQuantifier():
         raise NotImplementedError("""If implemented, this should return a
                                   visual representation of the qunatifier's
                                   state.""")
-    
+
+
 
 class MotionDetector():
+
+    def __init__(self, source, threshold, quantifier, quant_args={},
+                 doRecord=True, showWindows=True):
+
+        self.capture = source
+        self.running = True
+
+        #Internal state common to all detectors
+        #Used for motion detection itself
+        self.frame_no = 1 #Frame counter
+
+        _,init_frame = self.capture.read() #Take a frame to init recorder 
+        if DOWNSAMPLE:
+            while max(init_frame.shape[0:2]) > MIN_WIDTH * 2:
+                init_frame = cv2.pyrDown(init_frame)
+        
+
+        self.quant = quantifier(init_frame, **quant_args)
+
+        self.cooldown = 0
+
+        self.doRecord = doRecord #Either or not record the moving object
+        self.show = showWindows #Either or not show the 2 windows
+        
+
+        if doRecord:
+            self.writer = self.initRecorder()
+        
+        self.trigger_time = 0 #Hold timestamp of the last detection
+        
+        self.threshold = threshold
+        if showWindows:
+            cv2.namedWindow("Image")
+            cv2.createTrackbar("Detection treshold: ",
+                              "Image",
+                              int(self.threshold * 100),
+                              100,
+                              lambda v: self.onChange({'threshold': v / 100.}))
+    
+    #This is only really used for interactivity.
+    def onChange(self, keyvals):
+        for k,v in keyvals.iteritems():
+            setattr(self, k, v)
+
+    def onInterrupt(self, sig, frame):
+        self.running = False
+
+
+    #Not sure I even want to use this
+    def initRecorder(self): #Create the recorder
+        raise NotImplementedError("This relies on the old interface.")
+        codec = cv.CV_FOURCC('M', 'J', 'P', 'G') #('W', 'M', 'V', '2')
+        writer = cv.CreateVideoWriter(datetime.now().strftime("%b-%d_%H_%M_%S")+".wmv",
+                                         codec, 5, cv.GetSize(self.frame), 1)
+        #FPS set to 5 because it seems to be the fps of my cam but should be ajusted to your needs
+        self.font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 2, 8) #Creates a font
+
+        return writer
+
+    def run(self):
+        #raise NotImplementedError("Subclasses must implement this method")
+        while self.running:
+            log.debug("Frame: {}".format(self.frame_no))
+            log.debug("Movement: {}".format(self.quant.motion_level))            
+
+            read_ok, this_frame = self.capture.read()
+            if not read_ok:
+                log.debug("Out of frames, breaking.")
+                self.running = False
+                break
+                break
+
+            if DOWNSAMPLE:
+                while max(this_frame.shape[0:2]) > MIN_WIDTH * 2:
+                    this_frame = cv2.pyrDown(this_frame)
+            
+            self.quant.step(this_frame)
+
+            if self.quant.motion_level > self.threshold:
+                if self.cooldown == 0:
+                    log.info("Start: {}".format(self.frame_no))#start recording
+                self.cooldown = COOLDOWN_FRAMES
+
+            if self.cooldown > 0:
+                pass#write a frame
+                if self.cooldown == 1:
+                    log.info("Stop: {}".format(self.frame_no))
+                self.cooldown -= 1                                
+            
+            if self.show:
+                vis_frame = self.visualize()
+                cv2.imshow("Image", vis_frame)
+
+                c=cv2.waitKey(1) % 0x100
+                if c==27 or c == 10: #Break if user enters 'Esc'.
+                    self.running = False
+                    
+            self.frame_no += 1
+ 
+    #Embelling the qunatifier's visualization if necessary
+    def visualize(self):
+        vis_frame = self.quant.visualize()
+        if self.cooldown > 0:
+            width = vis_frame.shape[0]
+            radius = int(width * .04)
+            center = (int(radius*1.5), ) * 2
+            cv2.circle(vis_frame, center, radius, (0,0,255), thickness=-1)
+        return vis_frame
+        
+    def processImage(self, this_frame):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+
+class MotionDetectorOLD():
 
     def __init__(self, source, threshold, doRecord=True, showWindows=True):
 
--- MotionDetectorContours.py
+++ MotionDetectorContours.py
@@ -3,15 +3,10 @@ from datetime import datetime
 import time
 import sys
 
-from MotionDetector import MotionDetector, MotionQuantifier
+from MotionDetector import MotionDetector, MotionDetectorOLD, MotionQuantifier
 import numpy as np
 
 from scipy.ndimage import maximum_filter1d
-
-EWMA_ALPHA = 0.05 #Factor for exponentially accumulated motion blur
-
-DILATE_ITERS = 15 #Number of 3x3 dilation iters
-ERODE_ITERS = 10 #Number of 3x3 dilation iters
 
 
 CONTOUR_COLOR = (0,0,255)
@@ -19,132 +14,169 @@ HOLE_COLOR = (0, 255, 0)
 
 class ContourQuantifier(MotionQuantifier):
  
-    def __init__(self, init_frame):
-        MotionQuantifier.__init__(self, init_frame)
+    def __init__(self, init_frame, alpha=0.05,
+                 dilate_amt = .1, erode_amt = 0.05):
+        """
+        alpha - decay constant for ewma image
+        
+        """
+        MotionQuantifier.__init__(self)
 
-        self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.average_frame = self.frame * 1.0
-        self.absdiff_frame = self.frame * 1.0
-        self.previous_frame = self.frame * 1.0
-        self.area = self.gray_frame.size
-        self.currentsurface = 0
-        self.currentcontours = None
-    
+        self._scale = int((init_frame.shape[0] * init_frame.shape[1])**.5)
+
+        self.alpha = alpha
+        self.dilate_iters = int(dilate_amt * self._scale)
+        self.erode_iters = int(erode_amt * self._scale)
+        
+        self.slow_avg = init_frame * 1.0
+        self.step(init_frame)
+        
     def step(self, curframe):
-        cv2.GaussianBlur(curframe, (3,3), sigmaX=0, sigmaY=0, dst=curframe) #Remove false positives
-        cv2.accumulateWeighted(curframe, self.average_frame, EWMA_ALPHA) #Compute the average
+        #Not necessary with downsampled data
+        #cv2.GaussianBlur(curframe, (3,3), sigmaX=0, sigmaY=0, dst=curframe) #Remove false positives
         
-        self.previous_frame = self.average_frame.astype('u1') #Convert back to 8U frame
         
-        self.absdiff_frame = cv2.absdiff(curframe, self.previous_frame) # moving_average - curframe
+#        CURRENT:
+        #incorporate new input to EWMA
+        #put ewma into previous_frame
+        #put difference between input and prev into absdiff
+        #convert diff to gray, put into gray_frame
+        #apply threshold to gray image
+        #dilate gray
+        #erode gray
+        
+        #find countours
+        #sum area of contours
+        #normalize area, put into motion_level
+        #return motion_level
+        
+        
+#        New:
+        #incorporate new input to EWMA
+        #put ewma into previous_frame
+        #put difference between input and prev into absdiff
+        #convert diff to gray, put into gray_frame
+        #apply threshold to gray image
+        #dilate gray
+        #erode gray
+        
+        #find countours
+        #sum area of contours
+        #normalize area, put into motion_level
+        #return motion_level
+        cv2.accumulateWeighted(curframe, self.slow_avg, self.alpha) #Compute the average
+        
+        abs_diff = cv2.absdiff(curframe, self.slow_avg.astype('u1')) # moving_average - curframe
 
-        self.gray_frame = cv2.cvtColor(self.absdiff_frame, cv2.COLOR_BGR2GRAY) #Convert to gray
-        _,self.gray_frame = cv2.threshold(self.gray_frame, 50, 255, cv2.THRESH_BINARY)
+        abs_diff_gray = cv2.cvtColor(abs_diff, cv2.COLOR_BGR2GRAY) #Convert to gray
 
-        self.gray_frame = cv2.dilate(self.gray_frame,
-                                     np.ones((3,3),np.uint8),
-                                     iterations = DILATE_ITERS) #to get object blobs
+        cv2.threshold(abs_diff_gray, 50, 255, cv2.THRESH_BINARY,
+                      dst = abs_diff_gray)
 
-        self.gray_frame = cv2.erode(self.gray_frame,
-                                     np.ones((3,3),np.uint8),
-                                     iterations = ERODE_ITERS) #to get object blobs
+        cv2.dilate(abs_diff_gray, kernel=None, iterations=self.dilate_iters,
+                   dst = abs_diff_gray)
+        
+        cv2.erode(abs_diff_gray, kernel=None, iterations=self.erode_iters,
+                  dst = abs_diff_gray)
 
-        _,contours, tree = cv2.findContours(self.gray_frame,
-                                          mode = cv2.RETR_EXTERNAL,
-                                          method = cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, tree = cv2.findContours(abs_diff_gray,
+                                             mode=cv2.RETR_EXTERNAL,
+                                             method=cv2.CHAIN_APPROX_SIMPLE)
+
         moving_area = sum(map(cv2.contourArea, contours))
 
-        self.contours = contours
-        self.contour_tree = tree #Save contours
-        avg = 1.0 * moving_area / self.area #Calculate the average of contour area on the total size
+        #For visualiztaion
+        self._contours = contours #useful only for visualization
+        self._contour_tree = tree #Save contours
+        self._lastframe = curframe
+        
+        self.motion_level = 1.0 * moving_area / abs_diff_gray.size
 
-        self.motion_level = avg
         return self.motion_level
-
-class MotionDetectorAdaptative(MotionDetector):
-
-    def __init__(self, source, threshold=0.2, doRecord=True, showWindows=True):
-        MotionDetector.__init__(self, source=source, threshold=threshold,
-                                doRecord=doRecord, showWindows=showWindows)
     
-        self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.average_frame = self.frame * 1.0
-        self.absdiff_frame = self.frame * 1.0
-        self.previous_frame = self.frame * 1.0
-        self.area = self.gray_frame.size
-
-        self.currentsurface = 0
-        self.currentcontours = None
-
-    def processImage(self, curframe):
-
-        cv2.GaussianBlur(curframe, (3,3), sigmaX=0, sigmaY=0, dst=curframe) #Remove false positives
-        cv2.accumulateWeighted(curframe, self.average_frame, EWMA_ALPHA) #Compute the average
-        
-        self.previous_frame = self.average_frame.astype('u1') #Convert back to 8U frame
-        
-        self.absdiff_frame = cv2.absdiff(curframe, self.previous_frame) # moving_average - curframe
-
-        self.gray_frame = cv2.cvtColor(self.absdiff_frame, cv2.COLOR_BGR2GRAY) #Convert to gray
-        _,self.gray_frame = cv2.threshold(self.gray_frame, 50, 255, cv2.THRESH_BINARY)
-
-        self.gray_frame = cv2.dilate(self.gray_frame,
-                                     np.ones((3,3),np.uint8),
-                                     iterations = DILATE_ITERS) #to get object blobs
-
-        self.gray_frame = cv2.erode(self.gray_frame,
-                                     np.ones((3,3),np.uint8),
-                                     iterations = ERODE_ITERS) #to get object blobs
-
-
-        self.motion_level = self.somethingHasMoved()
-            
-    def somethingHasMoved(self):
-        # Find contours
-        _, contours, tree = cv2.findContours(self.gray_frame,
-                                          mode = cv2.RETR_EXTERNAL,
-                                          method = cv2.CHAIN_APPROX_SIMPLE)
-        
-        moving_area = sum(map(cv2.contourArea, contours))
-
-        self.contours = contours
-        self.contour_tree = tree #Save contours
-        avg = 1.0 * moving_area / self.area #Calculate the average of contour area on the total size
-
-        return avg
-
     def visualize(self):
-        vis_frame = self.frame.copy()
+        vis_frame = self.slow_avg.astype('u1')
 
-        cv2.drawContours(vis_frame, self.contours,
-                        hierarchy=self.contour_tree,
-                        contourIdx=-1,
-                        color=CONTOUR_COLOR, thickness=2)
-
+        mask = vis_frame[..., 0].copy()
+        cv2.drawContours(mask, self._contours, -1, 255, cv2.FILLED);
+        mask = (mask == 255)
+        vis_frame[mask] = self._lastframe[mask]
+        
+        cv2.drawContours(vis_frame, self._contours,
+                hierarchy=self._contour_tree,
+                contourIdx=-1,
+                color=CONTOUR_COLOR, thickness=2)
+        
         return vis_frame
 
 
-def get_motion_series(capture):
+def get_motion_series(capture, max_width = 256, frame_skip = 1):
     n_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
     motion_series = np.zeros(n_frames)
     
-    quant = ContourQuantifier(capture.read()[1])
-    motion_series[1:] = [quant.step(capture.read()[1]) for i in range(1,n_frames)]
-#    for i in range(1, n_frames):
-#        motion_series[i] = quant.step(capture.read()[1])
-#        print 100 * i // n_frames
+    frame = capture.read()[1]
+    if max_width:
+        downsample_factor = np.log2(max(frame.shape[0:2]) / max_width)
+        downsample_factor = max(0,int(downsample_factor))
+    
+    def downsample(f):
+        for n in range(downsample_factor):
+            f = cv2.pyrDown(f)
+        return f
+    
+    quant = ContourQuantifier(downsample(frame))
+    #motion_series[1:] = [quant.step(downsample(capture.read()[1])) for i in range(1,n_frames)]
+    for i in range(1, n_frames):
+        new_frame = capture.read()[1]
+        if i%frame_skip == 0:
+            motion_series[i] = quant.step(downsample(new_frame))
+        else:
+            motion_series[i] = motion_series[i-1]
+
+        if i%(n_frames // 100) == 0:
+            print i
     return motion_series
 
 
 def get_clips(series, threshold, warmup = 30, cooldown = 30):    
-    maximum_filter1d(series, warmup+cooldown+1, origin=(-warmup + cooldown)/2)
-    moving_frames = (series > threshold).astype('float')
+    maxed_series = maximum_filter1d(series,
+                                    warmup+cooldown+1,
+                                    origin=(-warmup + cooldown)/2)
+    moving_frames = (maxed_series > threshold).astype('float')
     on_off = np.diff(moving_frames)
     starts = np.where(on_off == 1)[0]
     stops  = np.where(on_off == -1)[0]
 
     return zip(starts, stops)
-    
+
+from subprocess import Popen
+import os
+import tempfile
+def split_vid(fname, clip_bounds):
+    tmpdir = tempfile.mkdtemp()
+    cmd_base = ['mencoder',
+                fname,
+                '-oac','pcm',
+                '-ovc','copy']
+    for i, cb in enumerate(clip_bounds):
+        this_file = 'clip_' + str(i).zfill(5) +'.'+ fname.split('.')[-1]
+        this_out = os.path.join(tmpdir, this_file)
+        this_cmd = ['-ss', str(cb[0]),
+                    '-endpos', str(cb[1]),
+                    '-o', this_out]
+        
+        proc = Popen(cmd_base + this_cmd)
+        proc.wait()
+
+
+def make_edit(fname):
+    #Get FPS
+    #read infile @ 15FPS, low res
+    #get motion time series
+    #convert to clips
+    #use mencoder to extract segments
+    #use mencoder to stitch together segments
+    pass
 if __name__=="__main__":
     fname = sys.argv[-1]
 
@@ -153,6 +185,6 @@ if __name__=="__main__":
     else:
         source = cv2.VideoCapture(sys.argv[-1])
 
-    detector = MotionDetectorAdaptative(source, threshold = 0.02,
-                                        doRecord=False, showWindows=True)
-    detector.run()
+    #detector = MotionDetectorAdaptative(source, threshold = 0.02,
+    #                                    doRecord=False, showWindows=True)
+    #detector.run()
